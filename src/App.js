33d57d8937852ebf81070c3dee2e14e7733ad6e7
jisbruzzi <jose.sbru@gmail.com>
2018-06-01 22:52:02 -0300

    un par de cositas

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'typeface-roboto'
+
+import AppBar from './AppBar.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AppBar />
+        <br/>
+        <br/>
+        <br/>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <br/>
+        p
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        hola
+        <br/>
+        <br/>
+        <br/>
+        v
+        <br/>
+        <br/>
+        v
+        v
+        v
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        v
+        v
+        v
+        v
+        <br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        aaasss
+
+      </div>
+    );
+  }
+}
+
+/*
+<header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        */
+
+export default App;
---
+++ src/AppBar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import SearchIcon from '@material-ui/icons/Search';
+import { IconButton } from '@material-ui/core';
+
+function SimpleAppBar(props) {
+  let estiloToolbar={
+      display:"flex",
+      justifyContent:"space-between"
+  }
+  return (
+        <AppBar position="fixed" color="default">
+            <Toolbar style={estiloToolbar}>
+                <Typography variant="title" color="inherit">
+                    Cocinalo!
+                </Typography>
+                <div>
+                    <IconButton aria-label="carrito">
+                        <ShoppingCartIcon />
+                    </IconButton>
+                    <IconButton aria-label="buscar">
+                        <SearchIcon />
+                    </IconButton>
+
+                </div>
+          
+          
+            </Toolbar>
+        </AppBar>
+  );
+}
+//<Button color="inherit"><Icon><></Button>
+
+SimpleAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default SimpleAppBar;//withStyles(styles)(SimpleAppBar);
