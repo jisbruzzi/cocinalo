660a6bec6920001b369690ec955eee86916fa835
jisbruzzi <jose.sbru@gmail.com>
2018-06-20 15:34:40 -0300

    Modifico el algoritmo que trae sugerencias y el de busqueda busca por categorias

--- src/Proxy.js
+++ src/Proxy.js
@@ -36,13 +36,14 @@ class Proxy {
         let matchTitulo=appdata.platos.filter((plato)=>parecidas(plato.title,consulta))
         let matchAutor=appdata.platos.filter((plato)=>parecidas(plato.author,consulta))
         let matchDescripcion=appdata.platos.filter((plato)=>parecidas(plato.descripcion,consulta))
-        let ret=[].concat(matchTitulo).concat(matchAutor).concat(matchDescripcion)
+        let matchCategoria=appdata.platos.filter((plato)=>parecidas(plato.categoria,consulta))
+        let ret=[].concat(matchTitulo,matchAutor,matchDescripcion,matchCategoria)
         return ret
       }
 
       return new Promise(function(resolve,reject){
-        let matchEntera=getPlatosConsultaRealmente(consulta)
-        let matchPalabra=consulta.split(" ").map(getPlatosConsultaRealmente).reduce((a,b)=>a.concat(b),[])
+        let matchEntera=getPlatosConsultaRealmente(consulta)//frase entera
+        let matchPalabra=consulta.split(" ").map(getPlatosConsultaRealmente).reduce((a,b)=>a.concat(b),[])//cada palabra
         let ret=[].concat(matchEntera).concat(matchPalabra).concat(appdata.platos)
         
         ret=ret.filter((item,pos,self)=>{
@@ -57,24 +58,72 @@ class Proxy {
           return str.toLocaleLowerCase().replace(".","").replace(",","").replace(";","")
         }
 
+        function atributoArreglado(atributo){
+          return appdata.platos
+            .map((p)=>arreglar(p[atributo]))
+        }
+
         busqueda=arreglar(busqueda)
         let palabra=""
-        let ret=[]
         let platos=appdata.platos
-        
-        let titulosMatch=platos
-          .map((p)=>arreglar(p.title))
-          .filter((titulo)=>titulo.includes(busqueda))
-        let autoresMatch=platos
-          .map((p)=>arreglar(p.author))
-          .filter((author)=>author.includes(busqueda))
-        let descripcionesMatch=platos
-          .map((p)=>arreglar(p.descripcion))
-          .filter((descripcion)=>descripcion.includes(busqueda))
 
+        
         let palabras=busqueda.split(" ")
         let restoPalabras=palabras.slice(0,palabras.length-1)
         let ultimaPalabra=palabras[palabras.length-1]
+        let rePalabraTerminada=new RegExp("("+busqueda+" [^ ]+)")
+        let rePalabraNoTerminada=new RegExp("("+busqueda+"[^ ]+)")
+
+        let restoFrase=restoPalabras.join(" ")
+
+        function sugeridorPalabraTerminada(texto){
+          let resultado=rePalabraTerminada.exec(texto)
+          if(resultado==null){
+            return null
+          }else{
+            return resultado[1]
+          }
+        }
+
+        function sugeridorPalabraNoTerminada(texto){
+          let resultado=rePalabraNoTerminada.exec(texto)
+          if(resultado==null){
+            return null
+          }else{
+            return resultado[1]
+          }
+        }
+
+        function sugerenciasAtributo(atributo,sugeridor){
+          return atributoArreglado(atributo).map(sugeridor).filter((s)=>s!=null)
+        }
+
+        function sugerenciasAtributoMultiples(atributo){
+          let sugTerminada=sugerenciasAtributo(atributo,sugeridorPalabraTerminada)
+          let sugNoTerminada=sugerenciasAtributo(atributo,sugeridorPalabraNoTerminada)
+          let sug=[].concat(sugTerminada,sugNoTerminada)
+          return sug
+        }
+
+        let sugTitulo=sugerenciasAtributoMultiples("title")
+        let sugAutor=sugerenciasAtributoMultiples("author")
+        let sugDescripcion=sugerenciasAtributoMultiples("descripcion")
+        let sugCategoria=sugerenciasAtributoMultiples("categoria")
+
+        let ret= [].concat(sugTitulo,sugAutor,sugDescripcion,sugCategoria)
+
+        ret=ret.filter((item,pos,self)=>{
+          return self.indexOf(item)==pos
+        })
+        resolve(ret.map(arreglar))
+
+        /*
+        
+        let titulosMatch=atributoIncluye("title",busqueda)
+        let autoresMatch=atributoIncluye("author",busqueda)
+        let descripcionesMatch=atributoIncluye("descripcion",busqueda)
+
+        
         
         if(busqueda.includes(" ")){//búsqueda para más de una palabra, esto va arriba de todo
           ret=ret.concat(titulosMatch).concat(autoresMatch)
@@ -93,6 +142,7 @@ class Proxy {
         })
 
         resolve(ret.map((s)=>arreglar(s)))
+        */
       })
     }
   }
